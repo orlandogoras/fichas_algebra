929db42dd248df47f151886ffcb5014da0766f99
orlandogoras <71925076+orlandogoras@users.noreply.github.com>
2025-03-24 10:22:47 -0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1742829323,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/fondo-sheet0.png",
+		"images/x_cuadrado-sheet0.png",
+		"images/x_ficha-sheet0.png",
+		"images/1_ficha-sheet0.png",
+		"images/tablero-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
